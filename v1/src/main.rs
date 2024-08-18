dc89338ad888fb7d84ac64891d874b76e357494e
Carlos Jardim <cj.jardim6@gmail.com>
2024-08-19 00:25:24 +0200

    speed

--- v1/src/main.rs
+++ v1/src/main.rs
@@ -1,6 +1,6 @@
 use dotenv::dotenv;
 use tokio::task;
-use tokio::time::{sleep, Duration};
+use tokio::time::{sleep, Duration, Instant};
 
 mod gpu_collector;
 mod handlers;
@@ -18,16 +18,31 @@ async fn main() {
         // Task for sending GPU data periodically
         task::spawn(async move {
             loop {
+                let start_time = Instant::now();
+
                 if let Ok(gpu_data_list) = gpu_collector::get_gpu_data() {
                     for gpu_data in gpu_data_list {
-                        println!("GPU Data: {:?}", gpu_data); // Optional: For local debug logging
+                        // Log the GPU data being sent
+                        println!("Sending GPU Data: {:?}", gpu_data);
+
                         // Send the GPU data over the WebSocket connection
                         websocket_handler::send_gpu_data(&mut websocket_conn, &gpu_data).await;
                     }
                 } else {
                     eprintln!("Failed to get GPU data");
                 }
-                sleep(Duration::from_secs(10)).await;
+
+                let elapsed_time = start_time.elapsed();
+                println!("Data collection and sending took: {:.2?}", elapsed_time);
+
+                // Adjust the sleep to ensure approximately 1 second between sends
+                let sleep_duration = if elapsed_time < Duration::from_secs(1) {
+                    Duration::from_secs(1) - elapsed_time
+                } else {
+                    Duration::from_secs(0)
+                };
+
+                sleep(sleep_duration).await;
             }
         });
     } else {
